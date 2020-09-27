ad4b97fcd5042d31af2ec46dc68db8488d2886e5
李志麟 <1825706671@qq.com>
2020-09-27 22:49:39 +0800

    sell-massage

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+// 引入字体
+import '@/assets/fonts/iconfont.css'
 
 Vue.config.productionTip = false
 
